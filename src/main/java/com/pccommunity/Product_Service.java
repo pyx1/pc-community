3ac71c91bd9ab1b1a667ed748768fcdf76b1ac79
pyx1 <74534957+pyx1@users.noreply.github.com>
2021-04-29 15:44:04 +0200

    To funciona

--- src/main/java/com/pccommunity/Product_Service.java
+++ src/main/java/com/pccommunity/Product_Service.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-//Prueba de cambios
 
 @Service
 public class Product_Service {
