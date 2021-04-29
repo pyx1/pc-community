9f0cd5ca9f1006bc88c74b74c3cc1f806493a8bd
pyx1 <74534957+pyx1@users.noreply.github.com>
2021-04-29 14:51:12 +0200

    Revert "Master"

--- src/main/resources/static/js/admin/new-product.js
+++ src/main/resources/static/js/admin/new-product.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function add(){
     var name = document.getElementById('name').value;
     var imagesource = document.getElementById('imagesource').value;
@@ -22,29 +21,4 @@ function add(){
     client.setRequestHeader("Content-type", "application/json");
     var body = JSON.stringify(item);
     client.send(body);
-=======
-function add(){
-    var name = document.getElementById('name').value;
-    var imagesource = document.getElementById('imagesource').value;
-    var description = document.getElementById('description').value;
-    var price = document.getElementById('price').value;
-    var banner1 = document.getElementById('banner1').value;
-    var banner2 = document.getElementById('banner2').value;
-    var banner3 = document.getElementById('banner3').value;
-    var stock = document.getElementById('stock').value;
-    var category = document.getElementById('category').value;
-
-    
-    var item = {name: name, description: description,imageSource:imagesource, bannerSource1:banner1, bannerSource2:banner2, bannerSource3:banner3 , price: price, category:category, stock: stock};
-    console.log(category);
-    var client = new XMLHttpRequest();
-    client.responseType = "json";
-    client.addEventListener('load', function(){
-        window.location="/admin/";
-    });
-    client.open("POST", "/admin/productos");
-    client.setRequestHeader("Content-type", "application/json");
-    var body = JSON.stringify(item);
-    client.send(body);
->>>>>>> 74a060ef41211405c3b2d9186dd4e8562e6cf7cf
 }
